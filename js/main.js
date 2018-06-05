2dcfb06c730382ce6c91256f7fb7155831d236ae
avernikov <avernikov@synack.com>
2018-06-04 17:44:14 -0700

    fix: add alt text to describes the content of the image

--- js/main.js
+++ js/main.js
@@ -106,6 +106,8 @@ updateRestaurants = () => {
   })
 }
 
+
+
 /**
  * Clear current restaurants, their HTML and remove their map markers.
  */
@@ -146,6 +148,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = restaurant.name + " Restaurant";
   li.append(image);
 
   const name = document.createElement('h1');
