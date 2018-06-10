1bb5500fc7178e294eba6b101079996be81773d1
avernikov <avernikov@synack.com>
2018-06-09 18:53:44 -0700

    fix: accessibility fix for coloring scheme, headers
    Accessibility fix for coloring scheme, headers and main css cleanup of commented lines

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -119,14 +119,17 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
 createReviewHTML = (review) => {
   const li = document.createElement('li');
   const name = document.createElement('p');
+  name.setAttribute("id", "reviewer-name");
   name.innerHTML = review.name;
   li.appendChild(name);
 
   const date = document.createElement('p');
+  date.setAttribute("id", "review-date");
   date.innerHTML = review.date;
   li.appendChild(date);
 
   const rating = document.createElement('p');
+  rating.setAttribute("id", "review-rating");
   rating.innerHTML = `Rating: ${review.rating}`;
   li.appendChild(rating);
 
